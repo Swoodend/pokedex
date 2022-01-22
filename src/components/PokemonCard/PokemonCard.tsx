1392152ef6a63f0268990551f952e664563fdef1
swoodend <swoodend@shinydocs.com>
2022-01-21 23:27:03 -0500

    rough in PokemonCard component

---
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface PokemonCardProps {
+};
+
+// todo - this will become the main display component when we successfully fetch pokemon data (SW)
+const PokemonCard = () => {
+
+    return (
+        <div>I am a pokemon card!</div>
+    );
+};
+
+export default PokemonCard
