a8b7e5d4ca1cebda0bc74e29f76ab2d9e9f2e659
swoodend <swoodend@shinydocs.com>
2022-01-22 11:08:04 -0500

    remove opinionated styling from the SearchBar component so it can be reused throughout the app

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -10,13 +10,14 @@ interface SearchBarProps {
 
 const SearchBar = (props: SearchBarProps) => {
     const {
-        placeholder = '',
+        placeholder = 'Charizard, Zapdos, etc...',
         value = '',
         onSearch,
         onChange,
     } = props;
 
     const onFormSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
         onSearch(event, value);
     }
 
@@ -26,24 +27,22 @@ const SearchBar = (props: SearchBarProps) => {
     }
 
     return (
-        <form className="w-full flex md:justify-center" onSubmit={onFormSubmit} role="search">
-            <div className="h-full flex w-full xl:justify-center xl:w-2/3 relative">
-                <input 
-                    id="search"
-                    type="text" 
-                    placeholder="Charizard, Zapdos, etc..." 
-                    value={value} 
-                    onChange={onInputChange}
-                    className="w-full pl-6 h-12 outline-none rounded-full text-lg font-md border border-gray-400
-                     placeholder-gray-600 placeholder-opacity-60 focus:border-blue-600"
-                />
-                <button 
-                    type="submit" 
-                    className="h-8 w-8 absolute top-2 right-4 rounded-full ring-offset-2 focus:outline-none focus:ring-1 focus:ring-blue-600 transform transition active:scale-95"
-                >
-                    <SearchIcon className="h-8 w-8 rounded-l-none text-gray-600 rounded-full"/>
-                </button>
-            </div>
+        <form className="w-full relative" onSubmit={onFormSubmit} role="search">
+            <input
+                id="search"
+                type="text"
+                placeholder={placeholder}
+                value={value}
+                onChange={onInputChange}
+                className="w-full pl-6 h-12 outline-none rounded-full text-lg font-md border border-gray-400
+                        placeholder-gray-600 placeholder-opacity-60 focus:border-blue-600"
+            />
+            <button
+                type="submit"
+                className="h-8 w-8 absolute top-2 right-4 rounded-full ring-offset-2 focus:outline-none focus:ring-1 focus:ring-blue-600 transform transition active:scale-95"
+            >
+                <SearchIcon className="h-8 w-8 rounded-l-none text-gray-600 rounded-full" />
+            </button>
         </form>
     );
 }
