a3e809005564737bfbf150d74b4e5351a2e99db2
swoodend <swoodend@shinydocs.com>
2022-01-21 23:17:27 -0500

    render a showcase page after user executes a query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useSearchParams } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Favourites from './pages/Favourites/Favourites';
+import Showcase from './pages/Showcase/Showcase';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="favourites" element={<Favourites />}/>
-    </Routes>
-  );
+    const [params,] = useSearchParams();
+    const search = params.get('search');
+
+    return (
+        <Routes>
+            <Route path="/" element={search ? <Showcase search={search} /> : <Home />} />
+            <Route path="favourites" element={<Favourites />} />
+        </Routes>
+    );
 }
 
 export default App;
